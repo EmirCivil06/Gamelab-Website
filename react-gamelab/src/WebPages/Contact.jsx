fcf7a9665ceb83ce2698f8f7a210fe90e9529bf7
Emir Civil <emir_civil@hotmail.com>
2025-10-09 00:10:52 +0300

    Implementations have been made

--- react-gamelab/src/WebPages/Contact.jsx
+++ react-gamelab/src/WebPages/Contact.jsx
@@ -5,31 +5,45 @@ import { FaEnvelope } from "react-icons/fa";
 export default function Contact() {
   return (
     <>
+      {/* Bu arka plan elemanları akışın dışında kalabilir */}
       <div className="contact-bg"></div>
       <div id="bg-image"></div>
-      <div id="contact-us-text"><p id="contact-us-text">Ne Duruyorsun? Haydi!</p> <br></br> <p>GameLab topluluğu olarak seni de aramızda görmekten mutluluk duyarız!</p></div>
-      <ContactBar />
-      <span id="span1">ya da...</span>
-      <div className="contact-us-dm">
-        Bize Ulaşın! &#8594;
-        <a
-          href="mailto: emir_civil@hotmail.com"
-          style={{ display: "contents" }}
-        >
-          <FaEnvelope
-            size={18}
-            className="email-icon"
-            color="#1d9bf0"
-            style={{
+      
+      {/* frontend rezalet a*k */}
+      {/* Tüm sayfa içeriğini bu ana taşıyıcıya alıyoruz */}
+      <div className="contact-container">
+        <div id="contact-us-text">
+          <p id="contact-us-text">Ne Duruyorsun? Haydi!</p> <br></br>{" "}
+          <p>
+            GameLab topluluğu olarak seni de aramızda görmekten mutluluk duyarız!
+          </p>
+        </div>
+        
+        <ContactBar />
+        
+        <span id="span1">ya da...</span>
+        
+        <div className="contact-us-dm">
+          Bize Ulaşın! &#8594;
+          <a
+            href="mailto: emir_civil@hotmail.com"
+            style={{ display: "contents" }}
+          >
+            <FaEnvelope
+              size={18}
+              className="email-icon"
+              color="#1d9bf0"
+              style={{
                 verticalAlign: "center",
                 position: "relative",
                 top: "4px",
                 marginLeft: "8px",
                 transition: "0.2s",
                 cursor: "pointer",
-            }}
-          />
-        </a>
+              }}
+            />
+          </a>
+        </div>
       </div>
     </>
   );
