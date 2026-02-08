42cf900f08caf3c5c46646d6d813743b69d23084
Emir Civil <emir_civil@hotmail.com>
2026-02-08 14:58:07 +0300

    Web sayfasının en güncel build'ı hazır

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,5 +1,7 @@
 import PocketBase from "pocketbase"
 
+// !! ŞU ANDA SADECE LOCALHOST ÜZERİNDEN AKTİF OLUR
+// !! DOMAINE AKTARILIRKEN ÖZEL KONFİGÜRASYON GEREKİR
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 export default pb;
